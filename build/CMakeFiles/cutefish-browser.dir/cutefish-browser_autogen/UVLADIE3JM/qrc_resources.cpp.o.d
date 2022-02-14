93ce690d2e32692b06a09d002e11f228a7868a30
Leap of Azzam <leapofazzam@gmail.com>
2022-02-14 16:10:30 +0000

    fixes

--- build/CMakeFiles/cutefish-browser.dir/cutefish-browser_autogen/UVLADIE3JM/qrc_resources.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/cutefish-browser.dir/cutefish-browser_autogen/UVLADIE3JM/qrc_resources.cpp.o: \
-  /home/leapofazzam/cutefish-browser/build/cutefish-browser_autogen/UVLADIE3JM/qrc_resources.cpp
